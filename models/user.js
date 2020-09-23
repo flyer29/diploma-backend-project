5ad08ef182c0a997d1913992aa137bbd290dff96
Andrey Borodin <flyer29@bk.ru>
2020-09-23 11:03:52 +0300

    создал модель user

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
+const UnauthorizedError = require('../errors/unauthorized-error');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  email: {
+    type: String,
+    validate: {
+      validator: (value) => validator.isEmail(value),
+      message: (props) => `Email ${props.value} не проходит валидацию!`,
+    },
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function (email, password, next) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new UnauthorizedError('Необходимо указать корректную почту или пароль');
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new UnauthorizedError('Необходимо указать корректную почту или пароль');
+          }
+          return user;
+        });
+    })
+    .catch(next);
+};
+
+module.exports = mongoose.model('user', userSchema);
