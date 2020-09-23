f7f472f6c835dcd6f3c684306d42154e6dbaebd1
Andrey Borodin <flyer29@bk.ru>
2020-09-23 14:44:48 +0300

    добавил логгер в проект

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const urlDoesNotExist = () => {
   throw new NotFoundError('Запрашиваемый ресурс не найден');
 };
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://localhost:27017/news-explorer', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
