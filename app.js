c785920785b5daecba77f2968bd7abc02368e207
Andrey Borodin <flyer29@bk.ru>
2020-10-27 02:36:29 +0300

    357839

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ mongoose.connect(apiLink, {
 });
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://flyer29.github.io/news-explorer-frontend');
+  res.header('Access-Control-Allow-Origin', 'https://flyer29.github.io/news-explorer-frontend/');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
